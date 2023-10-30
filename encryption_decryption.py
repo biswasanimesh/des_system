bec43d8e4099e6ed11ff652cb80a6572c07e528d
ANIMESH BISWAS <animesh.0067@gmail.com>
2023-10-31 00:00:53 +0800

    ready for encryption commit

--- encryption_decryption.py
+++ encryption_decryption.py
@@ -1,19 +1,60 @@
 from tkinter import *
 import pybase64
+from tkinter import messagebox
 
 root = Tk()
-# Channel Name
-root.title('Codemy.com - Encrypt/Decrypt BAse64')
-root.iconbitmap('c:/gui/codemy.ico')
 root.geometry("500x400")
 
 
 def clear():
-    pass
+    # Clear boxes
+    my_text.delete(1.0, END)
+    my_entry.delete(0, END)
+
+
 def encrypt():
-    pass
+    # Get text from text box
+    secret = my_text.get(1.0, END)
+    # Clear the text box
+    my_text.delete(1.0, END)
+
+    # Logic for password
+    if my_entry.get() == "turtle":
+        # Convert to byte
+        secret = secret.encode("ascii")
+        # Convert to base64
+        secret = pybase64.b64encode(secret)
+        # Convert it back to ascii
+        secret = secret.decode("ascii")
+        # Print to text box
+        my_text.insert(END, secret)
+
+    else:
+        # Flash a message if wrong password
+        messagebox.showwarning("Incorrect!", "Incorrect Password, Try Again!")
+
+
 def decrypt():
-    pass
+    # Get text from text box
+    secret = my_text.get(1.0, END)
+    # Clear the screen
+    my_text.delete(1.0, END)
+
+    # Logic for password
+    if my_entry.get() == "turtle":
+        # Convert to byte
+        secret = secret.encode("ascii")
+        # Convert to base64
+        secret = pybase64.b64decode(secret)
+        # Convert it back to ascii
+        secret = secret.decode("ascii")
+        # Print to text box
+        my_text.insert(END, secret)
+
+    else:
+        # Flash a message if wrong password
+        messagebox.showwarning("Incorrect!", "Incorrect Password, Try Again!")
+
 
 my_frame = Frame(root)
 my_frame.pack(pady=20)
@@ -22,7 +63,7 @@ enc_button = Button(my_frame, text="Encrypt", font=("Helvetica", 18), command=en
 enc_button.grid(row=0, column=0)
 
 dec_button = Button(my_frame, text="Decrypt", font=("Helvetica", 18), command=decrypt)
-dec_button.grid(row=0, column=1)
+dec_button.grid(row=0, column=1, padx=20)
 
 clear_button = Button(my_frame, text="Clear", font=("Helvetica", 18), command=clear)
 clear_button.grid(row=0, column=2)
@@ -36,7 +77,55 @@ my_text.pack(pady=10)
 password_label = Label(root, text="Enter Your Password...", font=("Helvetica", 14))
 password_label.pack()
 
-my_entry = Entry(root, font=("Helvetica", 18), width=36, show="*")
+my_entry = Entry(root, font=("Helvetica", 18), width=35, show="*")
 my_entry.pack(pady=10)
 
 root.mainloop()
+
+
+
+
+
+
+# from tkinter import *
+# import pybase64
+#
+# root = Tk()
+# # Channel Name
+# root.title('Codemy.com - Encrypt/Decrypt Base64')
+# root.iconbitmap('c:/gui/codemy.ico')
+# root.geometry("500x400")
+#
+#
+# def clear():
+#     pass
+# def encrypt():
+#     pass
+# def decrypt():
+#     pass
+#
+# my_frame = Frame(root)
+# my_frame.pack(pady=20)
+#
+# enc_button = Button(my_frame, text="Encrypt", font=("Helvetica", 18), command=encrypt)
+# enc_button.grid(row=0, column=0)
+#
+# dec_button = Button(my_frame, text="Decrypt", font=("Helvetica", 18), command=decrypt)
+# dec_button.grid(row=0, column=1)
+#
+# clear_button = Button(my_frame, text="Clear", font=("Helvetica", 18), command=clear)
+# clear_button.grid(row=0, column=2)
+#
+# enc_label = Label(root, text="Encrypt/Decrypt Text...", font=("Helvetica", 14))
+# enc_label.pack()
+#
+# my_text = Text(root, width=57, height=10)
+# my_text.pack(pady=10)
+#
+# password_label = Label(root, text="Enter Your Password...", font=("Helvetica", 14))
+# password_label.pack()
+#
+# my_entry = Entry(root, font=("Helvetica", 18), width=36, show="*")
+# my_entry.pack(pady=10)
+#
+# root.mainloop()
